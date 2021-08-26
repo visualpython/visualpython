f555527f07757f7974846b19fd4feb354b9e4df5
leeraejung <leeraejung@gmail.com>
2021-08-26 19:34:18 +0900

    #78 - 2nd modify visualpy, visualpy.bat: Add argument(ex: --pip3 )

--- bin/visualpy.bat
+++ bin/visualpy.bat
@@ -1,253 +1,331 @@
 @echo off
 SetLocal
-rem #==========================================================================
-rem # Filename : visualpy.bat
-rem # function : control Visual Python for windows
-rem # Creator  : BlackLogic - LJ
-rem # version  : 2.2
-rem # License  : GPLv3
-rem # Date     : 2020 07.30
-rem # MDate    : 2020 07.20
-rem #==========================================================================
+rem #
+rem #    Project Name    : Visual Python
+rem #    Description     : GUI-based Python code generator
+rem #    File Name       : visualpy.bat
+rem #    Author          : Black Logic - LJ
+rem #    Note            : Control Visual Python for windows
+rem #    License         : GPLv3 (GNU General Public License v3.0)
+rem #    Date            : 2021. 08. 14
+rem #    Change Date     :
+rem #
 
-rem ## setting variables
+rem #=========================================================================
+rem # Check Arguments
+rem #   - set VP_OPTION & PIP_T
+rem #=========================================================================
+set argc=0
+for %%x in (%*) do Set /a argc+=1
 
-set v_pip=pip
-where pip3 > nul 2>&1 && set v_pip=pip3
-
-set v_prod=visualpython
-(echo "%v_prod%" & echo.) | findstr /O . | more +1 | (set /P RESULT= & call exit /B %%RESULT%%)
-set /A v_prd_length=%ERRORLEVEL%-5
-set v_option=%1
-for /f "delims=, tokens=1*" %%i in ('%v_pip% show %v_prod% 2^>^&1 ^| find "Location"') do (
-set v_1=%%i)
-set v_path1=%v_1:~10%
-set v_str1=jupyter nbextension
-set v_str2=%v_prod%/src/main
-set v_srch=%v_pip% search %v_prod%
-set v_upgr=pip install %v_prod% --upgrade
-set v_unst=%v_pip% uninstall %v_prod%
-set v_ckup=%v_pip% list -o
-set v_show=%v_pip% show %v_prod%
-
-rem check env & setting path2
-where /q conda-env
-IF ERRORLEVEL 1 (
-    call :l_prt_be_line
-    ECHO Not a conda env.
-    set v_path2=%APPDATA%\jupyter\nbextensions\
-) ELSE (
-    call :l_prt_be_line
-    ECHO conda env.
-    set v_path2=%v_path1:~,-18%\share\jupyter\nbextensions\
+if %argc% == 1 (
+    set VP_OPTION=%1
+) else if %argc% == 2 (
+    set ARG1=%1
+    set ARG2=%2
+) else (
+    set VP_OPTION=
 )
 
-rem ## Main Block
-:l_main
-    IF /i "%v_option%"=="" goto :l_help
-
-    for %%i in (-h help)           do if /i %v_option% == %%i call :l_help
-    for %%i in (-e enable)         do if /i %v_option% == %%i call :l_enable
-    for %%i in (-d disable)        do if /i %v_option% == %%i call :l_disable
-    for %%i in (-i install)        do if /i %v_option% == %%i call :l_install
-    for %%i in (-up upgrade)       do if /i %v_option% == %%i call :l_upgrade
-    for %%i in (-v version)        do if /i %v_option% == %%i call :l_version
-    for %%i in (-ce checkext)      do if /i %v_option% == %%i call :l_check_extension
-    for %%i in (-cb checkbpl)      do if /i %v_option% == %%i call :l_check_visualpython
-    for %%i in (-un uninstall)     do if /i %v_option% == %%i goto :l_uninstall
-    goto :eof
-
-rem ## Function Block
-:l_help
-    echo.
-    echo usage: visualpy option
-    echo optional arguments:
-    echo  -h,  help        show this help message and exit
-    echo  -e,  enable      enable Visual Python
-    echo  -d,  disable     disable Visual Python
-    echo  -i,  install     install Visual Python extensions
-    echo  -un, uninstall   uninstall Visual Python packages
-    echo  -up, upgrade     upgrade Visual Python Package
-    echo  -v,  version     show Visual Python current version
-    echo.
-    goto :eof
-
-:l_check_extension
-    IF NOT EXIST "%v_path2%" call :l_prt_extensiondir
-    goto :eof
-
-:l_check_visualpython
-    IF EXIST "%v_path2%%v_prod%" (
-            set v_flag=1
-        ) ELSE (
-            set v_flag=2
-        )
-    goto :eof
-
-:l_install
-	rem "" is for envname with space.
-    IF EXIST "%v_path2%%v_prod%" ( call :l_prt_be_line
-                                   echo Check installed %v_prod% Path :
-                                   echo %v_path2%%v_prod%
-                                   call :l_prt_visualpythondir
-								   call :l_overwrite
-    ) ELSE ( call :l_copy_files
-             call :l_enable
+if %argc% == 2 (
+    if "%ARG1:~0,2%" == "--" (
+        set VP_OPTION=%ARG2%
+        set PIP_T=%ARG1:~2%
+    ) else (
+        set VP_OPTION=%ARG1%
+        set PIP_T=%ARG2:~2%
     )
+)
+
+rem #=========================================================================
+rem # Set variable
+rem #=========================================================================
+set PIP=pip
+where %PIP_T% > nul 2>&1 && set PIP=%PIP_T%
+
+set JP_NB=jupyter nbextension
+
+set VP_NAME=visualpython
+set VP_BIND=visualpython/src/main
+
+set PIP_UNINST=%PIP% uninstall %VP_NAME%
+set PIP_UPGRAD=%PIP% install %VP_NAME% --upgrade
+
+rem #=========================================================================
+rem # main block
+rem #=========================================================================
+:f_main
+    echo Package install command: %PIP%
+
+    IF /i "%VP_OPTION%"=="" goto :f_help
+    for %%i in (-v version)        do if /i %VP_OPTION% == %%i goto :f_version
+    for %%i in (-h help)           do if /i %VP_OPTION% == %%i goto :f_help
+
+    call :f_get_string_pipshow RES Location
+    set PATH_SRC=%RES%
+
+    call :f_get_extension_path RES
+    set PATH_DST=%RES%
+
+    for %%i in (-e enable)         do if /i %VP_OPTION% == %%i goto :f_enable
+    for %%i in (-d disable)        do if /i %VP_OPTION% == %%i goto :f_disable
+    for %%i in (-i install)        do if /i %VP_OPTION% == %%i goto :f_install
+    for %%i in (-un uninstall)     do if /i %VP_OPTION% == %%i goto :f_uninstall
+    for %%i in (-up upgrade)       do if /i %VP_OPTION% == %%i goto :f_upgrade
+
+    goto :f_help
+
     goto :eof
 
-:l_enable
-    call :l_prt_be_line
-    %v_str1% enable %v_str2%
-    call :l_prt_af_line
+rem #=========================================================================
+rem # Install Visual Python
+rem #=========================================================================
+:f_install
+    call :f_check_extension RES
+    rem # 1 = Jupyterr Extension is not actived
+    rem # 2 = visualpython does not exist
+    rem # 3 = visualpython exists
+
+    if %RES% == 1 (
+        call :f_print_not_extension
+    ) else if %RES% == 2 (
+        call :f_copy_files
+        call :f_enable
+    ) else if %RES% == 3 (
+        rem # overwrite
+        call :f_print_line1
+        echo Already exists Visual Python.
+        call :f_print_line1
+
+        call :f_disable
+        call :f_remove_files
+        call :f_copy_files
+        call :f_enable
+    )
+
     goto :eof
 
-:l_disable
-    call :l_prt_af_line
-    %v_str1% disable %v_str2%
-    call :l_prt_af_line
+rem #=========================================================================
+rem # Uninstall Visual Python
+rem #=========================================================================
+:f_uninstall
+    call :f_check_extension RES
+    rem # 1 = Jupyterr Extension is not actived
+    rem # 2 = visualpython does not exist
+    rem # 3 = visualpython exists
+
+    if %RES% == 2 (
+        call :f_print_line2
+        %PIP_UNINST%
+        call :f_print_line2
+    ) else if %RES% == 3 (
+        call :f_print_line1
+        call :f_disable
+        call :f_print_line2
+        call :f_remove_files
+        call :f_print_line2
+        %PIP_UNINST%
+        call :f_print_line1
+    )
+
     goto :eof
 
-:l_overwrite
-    rem if visaulpython 없는경우 install로 수행
-    rem else remove & install
-    call :l_disable
-    call :l_remove_files
-    call :l_copy_files
-    call :l_enable
+rem #=========================================================================
+rem # Upgrade Visual Python
+rem #=========================================================================
+:f_upgrade
+    call :f_print_line1
+    echo Running upgrade Visual Python
+    call :f_print_line2
+
+    rem # Get Visual Python version
+    call :f_get_string_pipshow RES Version
+    set VP_VERSION=%RES%
+
+    %PIP_UPGRAD%
+
+    rem # Get Visual Python new version
+    call :f_get_string_pipshow RES Version
+    set VP_VERSION_NEW=%RES%
+
+    if "%VP_VERSION%" == "%VP_VERSION_NEW%" (
+        call :f_print_line2
+        echo Already installed last Visual Python version.
+        call :f_print_line2
+        echo Installed version    : %VP_VERSION%
+        echo Last Release version : %VP_VERSION_NEW%
+        call :f_print_line1
+    ) else (
+        call :f_print_line2
+        call :f_disable
+        call :f_remove_files
+        call :f_copy_files
+        call :f_enable
+        call :f_print_line1
+    )
+
     goto :eof
 
-:l_copy_files
-    call :l_prt_be_line
-    echo source : %v_path1%\%v_prod%\
-    rem "" is for envname with space.
-    xcopy /q /y /e "%v_path1%\%v_prod%" "%v_path2%%v_prod%\"
-    echo target : %v_path2%%v_prod%\
-    call :l_prt_af_line
+rem #=========================================================================
+rem # Enable Visual Python
+rem #=========================================================================
+:f_enable
+    call :f_check_extension RES
+    if %RES% == 3 %JP_NB% enable %VP_BIND%
+
     goto :eof
 
-:l_remove_files
-    call :l_prt_be_line
-    echo Remove Visual Python Directories.
-    rem "" is for envname with space.
-    rmdir /s /q "%v_path2%%v_prod%"
-    call :l_prt_af_line
+rem #=========================================================================
+rem # Disable Visual Python
+rem #=========================================================================
+:f_disable
+    call :f_check_extension RES
+    if %RES% == 3 %JP_NB% disable %VP_BIND%
+
     goto :eof
 
-:l_upgrade
-    call :l_prt_af_line
-    echo Running upgrade visualpython.	
-    call :l_prt_af_line	
-	rem setting current version
-	for /f "tokens=*" %%i in ( '%v_show% 2^>^&1 ^| findstr /i Version') do (
-	set v_currentver=%%i
-	)
-	
-	rem running version upgrade
-	%v_upgr%
-	
-	rem setting last installed version
-	for /f "tokens=*" %%i in ( '%v_show% 2^>^&1 ^| findstr /i Version') do (
-	set v_lastver=%%i
-	)
-	set v_install=   Installed %v_currentver%
-	set v_last=Last Release %v_lastver%
-	
-    call :l_prt_af_line
-	
-	if not %v_lastver:~9% == %v_currentver:~9% (
-	    echo change visualpython extension files ...
-        call :l_disable
-        call :l_copy_files
-        call :l_enable
-	) else (
-	    echo Already installed last Visual Python version.
-		call :l_print_version
-	)
-    goto :eof
-		
-:l_version
-    rem call :l_prt_be_line
-    
-	for /f "tokens=*" %%i in ('%v_ckup% 2^>^&1 ^|  find /i "%v_prod%"') do ( set v_info=%%i )
+rem #=========================================================================
+rem # Visual Python version
+rem #=========================================================================
+:f_version
+    call :f_get_string_pipshow RES Version
+    set VP_VERSION=%RES%
+    echo Visual Python %VP_VERSION%
 
-	if "%v_info%."=="." ( 
-	     rem pass upgrade
-		 for /f "tokens=*" %%i in ( '%v_show% 2^>^&1 ^| findstr /i Version') do ( set v_last=%%i 
-
-		 set  v_last=Last Release %%i
-		 set  v_install=   Installed %%i
-		 set  v_ver_flag=0		 
-	      ) 
-	) else ( 
-	     rem run upgrade
-         for /f "tokens=2-3" %%i in ( 'echo %v_info%' ) do (
-                                                            rem echo Last release Version : %%j 
-                                                            rem echo Installed Version    : %%i 
-                                                            set  v_install=   Installed Version: %%i
-                                                            set  v_last=Last Release Version: %%j
-                                                            set  v_ver_flag=1 
-														) 
-	)
-	call :l_print_version
     goto :eof
 
-:l_print_version
-    call :l_prt_af_line
-	echo %v_install%
-	echo %v_last%
-    call :l_prt_af_line
-	rem echo %v_flag%
-    goto :eof	
-
-:l_prt_extensiondir
-    call :l_prt_be_line
-    echo Nbextension not activated
-    echo Plz install nbextension
-    call :l_prt_af_line
-    goto :eof
-
-:l_prt_visualpythondir
-    call :l_prt_af_line
-    echo Already exists Visual Python.
-    goto :eof
-
-:l_prt_notexists_visualpythondir
-    call :l_prt_be_line
-    echo Visual Python extension not installed.
-    call :l_prt_af_line
-    goto :eof
-
-:l_prt_be_line
+rem #=========================================================================
+rem # Help messages
+rem #=========================================================================
+:f_help
     echo.
+    echo usage: visualpy [option] [--pip3]
+    echo.
+    echo optional arguments:
+    echo   -h,  help       show this help message and exit
+    echo   -e,  enable     enable Visual Python
+    echo   -d,  disable    disable Visual Python
+    echo   -i,  install    install Visual Python extensions
+    echo   -ui, uninstall  uninstall Visual Python packages
+    echo   -up, upgrade    upgrade Visual Python Package
+    echo   -v,  version    show Visual version and exit
+    echo.
+    echo   --pip3          use pip3 [default: pip]
+    echo. 
+
+    goto :eof
+
+rem #=========================================================================
+rem # Copy Visual Python files
+rem #=========================================================================
+:f_copy_files
+    call :f_print_line1
+    echo Copy visualpython extension files ...
+    call :f_print_line2
+    echo Source Dir : %PATH_SRC%\%VP_NAME%
+    echo Target Dir : %PATH_DST%\%VP_NAME%
+    call :f_print_line1
+    xcopy /q /y /e "%PATH_SRC%\%VP_NAME%" "%PATH_DST%\%VP_NAME%\"
+
+    goto :eof
+
+rem #=========================================================================
+rem # Remove Visual Python files
+rem #=========================================================================
+:f_remove_files
+    call :f_print_line1
+    echo Remove Visual Python Directories.
+    rmdir /s /q "%PATH_DST%\%VP_NAME%"
+
+    goto :eof
+
+rem #=========================================================================
+rem # Check Visual Python files
+rem #   1 = Jupyterr Extension is not actived
+rem #   2 = visualpython does not exist
+rem #   3 = visualpython exists
+rem #=========================================================================
+:f_check_extension
+    if not exist "%PATH_DST%" (
+        set %~1=1
+    ) else if not exist "%PATH_DST%\%VP_NAME%" (
+        set %~1=2
+    ) else (
+        set %~1=3
+    )
+
+    goto :eof
+
+rem #=========================================================================
+rem # Get string(Version or Location) from pip show
+rem #   %~2 = Version or Location
+rem #=========================================================================
+:f_get_string_pipshow
+    set BUF1=
+    set RESULT=EMPTY
+
+    for /f "delims=: tokens=1*" %%i in ('%PIP% show %VP_NAME% 2^>^&1 ^| find "%~2"') do (set BUF1=%%j)
+
+    set RESULT=%BUF1: =%
+    set %~1=%RESULT%
+
+    goto :eof
+
+rem #=========================================================================
+rem # Get string(Jupyter nbextension path) from conda-env or jupyter
+rem #=========================================================================
+:f_get_extension_path
+    set BUF1=
+    set BUF2=
+    set RESULT=EMPTY
+
+    where /q conda-env
+    if "%ERRORLEVEL%" == "0" (
+        for /f "delims=* tokens=2" %%i in ('conda-env list 2^>^&1 ^| find "*"') do (set BUF1=%%i)
+    ) else (
+        for /f "tokens=*" %%i in ('jupyter --data-dir') do (set BUF2=%%i)
+    )
+
+    if not "%BUF1%" == "" (
+        set RESULT=%BUF1: =%\share\jupyter\nbextensions
+    ) else (
+        set RESULT=%BUF2: =%\nbextensions
+    )
+    set %~1=%RESULT%
+
+    goto :eof
+
+rem #=========================================================================
+rem # Print extension is not installed
+rem #=========================================================================
+:f_print_not_extension
+    echo Jupyter nbextension is not activated
+    echo Please install Jupyter nbextension
+    call :f_print_line1
+    echo for conda env
+    echo conda install -c conda-forge jupyter_contrib_nbextensions
+    echo jupyter contrib nbextension install --user
+    call :f_print_line2
+    echo for pip
+    echo %PIP% install -e jupyter_contrib_nbextensions
+    echo jupyter contrib nbextension install --user
+    call :f_print_line1
+
+    goto :eof
+
+rem #=========================================================================
+rem # Print line
+rem #=========================================================================
+:f_print_line1
     echo ==========================================================================================
     goto :eof
 
-:l_prt_af_line
-    echo ==========================================================================================
-    goto :eof
-
-:l_prt_line
+:f_print_line2
     echo ------------------------------------------------------------------------------------------
     goto :eof
 
+rem #=========================================================================
 
-:l_uninstall
-    IF EXIST "%v_path2%%v_prod%" (
-        call :l_disable
-        call :l_remove_files
-        echo "%v_path2%%v_prod%"
-        call :l_prt_af_line
-        %v_unst%
-    ) else (
-        call :l_prt_be_line
-        echo %v_path1:~,-18%\share\jupyter\nbextensions\
-        echo %v_path2%%v_prod%
-        call :l_prt_af_line
-        %v_unst%
-    )
+rem # End of file
 
-rem #==========================================================================
-rem #End of File
-rem #==========================================================================
 EndLocal
