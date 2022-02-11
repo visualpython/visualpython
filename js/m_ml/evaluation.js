2b10ecafe6dad2d33e4a2138114394261de681b3
minjk-bl <mjkim-bl@gmail.com>
2022-02-11 15:00:20 +0900

    ML > evaluation schema

--- js/m_ml/evaluation.js
+++ js/m_ml/evaluation.js
@@ -13,11 +13,12 @@
 // [CLASS] Evaluation
 //============================================================================
 define([
+    'text!vp_base/html/m_ml/evaluation.html!strip',
     'vp_base/js/com/com_util',
     'vp_base/js/com/com_Const',
     'vp_base/js/com/com_String',
     'vp_base/js/com/component/PopupComponent'
-], function(com_util, com_Const, com_String, PopupComponent) {
+], function(evalHTML, com_util, com_Const, com_String, PopupComponent) {
 
     /**
      * Evaluation
@@ -25,33 +26,132 @@ define([
     class Evaluation extends PopupComponent {
         _init() {
             super._init();
-            /** Write codes executed before rendering */
+            this.config.dataview = false;
+
+            this.state = {
+                modelType: 'clf',
+                predictData: 'pred',
+                targetData: 'y_test',
+                // classification
+                confusion_matrix: true, report: true, 
+                accuracy: false, precision: false, recall: false, f1_score: false,
+                // regression
+                coefficient: false, intercept: false, r_squared: true, 
+                mae: false, mape: false, rmse: true, scatter_plot: false,
+                ...this.state
+            }
         }
 
         _bindEvent() {
             super._bindEvent();
             /** Implement binding events */
             var that = this;
-            this.$target.on('click', function(evt) {
-                var target = evt.target;
-                if ($(that.wrapSelector()).find(target).length > 0) {
-                    // Sample : getDataList from Kernel
-                    vpKernel.getDataList().then(function(resultObj) {
-                        vpLog.display(VP_LOG_TYPE.DEVELOP, resultObj);
-                    }).catch(function(err) {
-                        vpLog.display(VP_LOG_TYPE.DEVELOP, err);
-                    });
-                }
+
+            // import library
+            $(this.wrapSelector('#vp_importLibrary')).on('click', function() {
+                com_interface.insertCell('code', 'from sklearn import metrics');
             });
+
+            // model type change
+            $(this.wrapSelector('#modelType')).on('change', function() {
+                let modelType = $(this).val();
+                that.state.modelType = modelType;
+
+                $(page).find('.vp-eval-box').hide();
+                $(page).find('.vp-eval-'+modelType).show();  
+            })
         }
 
         templateForBody() {
-            /** Implement generating template */
-            return 'This is sample.';
+            let page = $(evalHTML);
+
+            $(page).find('.vp-eval-box').hide();
+            $(page).find('.vp-eval-'+this.state.modelType).show();
+
+            return page;
         }
 
         generateCode() {
-            return "print('sample code')";
+            let code = new com_String();
+            let { 
+                modelType, predictData, targetData,
+                // classification
+                confusion_matrix, report, accuracy, precision, recall, f1_score,
+                // regression
+                coefficient, intercept, r_squared, mae, mape, rmse, scatter_plot
+            } = this.state;
+
+            //====================================================================
+            // Classfication
+            //====================================================================
+            if (modelType == 'clf') {
+                if (confusion_matrix) {
+                    code.appendLine("# Confusion Matrix");
+                    code.appendFormatLine('pd.crosstab({0}, {1}, margins=True)', targetData, predictData);
+                }
+                if (report) {
+                    code.appendLine("# Classification report");
+                    code.appendFormatLine('print(metrics.classification_report({0}, {1}))', targetData, predictData);
+                }
+                if (accuracy) {
+                    code.appendLine("# Accuracy");
+                    code.appendFormatLine('metrics.accuracy_score({0}, {1})', targetData, predictData);
+                }
+                if (precision) {
+                    code.appendLine("# Precision");
+                    code.appendFormatLine("metrics.precision_score({0}, {1}, average='weighted')", targetData, predictData);
+                }
+                if (recall) {
+                    code.appendLine("# Recall");
+                    code.appendFormatLine("metrics.recall_score({0}, {1}, average='weighted')", targetData, predictData);
+                }
+                if (f1_score) {
+                    code.appendLine("# F1-score");
+                    code.appendFormatLine("metrics.f1_score({0}, {1}, average='weighted')", targetData, predictData);
+                }
+            }
+
+            //====================================================================
+            // Regression
+            //====================================================================
+            if (modelType == 'rgs') {
+                if (coefficient) {
+                    code.appendLine("# Coefficient (scikit-learn only)");
+                    code.appendFormatLine('model.coef_');
+                }
+                if (intercept) {
+                    code.appendLine("# Intercept (scikit-learn only)");
+                    code.appendFormatLine('model.intercept_');
+                }
+                if (r_squared) {
+                    code.appendLine("# R square");
+                    code.appendFormatLine('metrics.r2_score({0}, {1})', targetData, predictData);
+                }
+                if (mae) {
+                    code.appendLine("# MAE(Mean Absolute Error)");
+                    code.appendFormatLine('metrics.mean_absolute_error({0}, {1})', targetData, predictData);
+                }
+                if (mape) {
+                    code.appendLine("# MAPE(Mean Absolute Percentage Error)");
+                    code.appendLine('def MAPE(y_test, y_pred):');
+                    code.appendLine('   return np.mean(np.abs((y_test - pred) / y_test)) * 100');
+                    code.appendLine();
+                    code.appendFormatLine('MAPE({0}, {1})', targetData, predictData);
+                }
+                if (rmse) {
+                    code.appendLine("# RMSE(Root Mean Squared Error)");
+                    code.appendFormatLine('metrics.mean_squared_error({0}, {1})**0.5', targetData, predictData);
+                }
+                if (scatter_plot) {
+                    code.appendLine('# Regression plot');
+                    code.appendFormatLine('plt.scatter({0}, {1})', targetData, predictData);
+                    code.appendFormatLine("plt.xlabel('{0}')", targetData);
+                    code.appendFormatLine("plt.ylabel('{1}')", predictData);
+                    code.appendLine('plt.show()');
+                }
+            }
+            // FIXME: as seperated cells
+            return code.toString();
         }
 
     }
