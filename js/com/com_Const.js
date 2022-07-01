48804938ba1d88aa9e926b49fe3dc0dff475172e
visualpython <visualpython.official@gmail.com>
2022-07-01 17:49:41 +0900

    deploy visualpython 2.2.8

--- js/com/com_Const.js
+++ js/com/com_Const.js
@@ -19,7 +19,7 @@ define ([
     class Constants { }
 
     Constants.TOOLBAR_BTN_INFO = {
-        HELP: "Visual Python 2.2.7"
+        HELP: "Visual Python 2.2.8"
         , ICON: "vp-main-icon"
         , ID: "vpBtnToggle"
         , NAME: "toggle-vp"
