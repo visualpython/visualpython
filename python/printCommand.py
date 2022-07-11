494d694846f4f25ab3efb06636d220861a5cd0da
Minku-koo <koomk97@naver.com>
2022-07-11 20:43:12 +0900

    first test minku.koo

--- python/printCommand.py
+++ python/printCommand.py
@@ -21,3 +21,6 @@ def _vp_print(command):
     with _vp_warnings.catch_warnings():
         _vp_warnings.simplefilter(action='ignore', category=FutureWarning)
         print(_vp_json.dumps(command, ensure_ascii=False, cls=_VpNpEncoder))
+
+
+# hello i am koo
