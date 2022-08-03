4a34b5e27a232c539c19d1b5f08b35c0f8d67755
minku.koo <corleone@kakao.com>
2022-08-04 02:30:20 +0900

    reorigin Try.js

--- js/m_logic/Try.js
+++ js/m_logic/Try.js
@@ -6,7 +6,7 @@
  *    Note            : Logic > try
  *    License         : GNU GPLv3 with Visual Python special exception
  *    Date            : 2021. 11. 18
- *    Change Date     : 2022. 08. 0x
+ *    Change Date     :
  */
 
 //============================================================================
@@ -31,38 +31,20 @@ define([
             this.state = {
                 ...this.state
             }
-
-            this.exceptClassName = 'vp-try-except-input';
-            this.addBtnIdName = 'vp-try-add-btn';
-            this.inputBoxText = 'Input Error Type';
             
             this._addCodemirror('code', this.wrapSelector('#code'));
         }
 
         _bindEvent() {
             super._bindEvent();
-            var page = new com_String();
-            let that = this;
             /** Implement binding events */
-            $(this.wrapSelector('#' + this.addBtnIdName)).on('click', function() {
-                alert("btn click!");
-                // add except input
-            });
         }
 
         templateForBody() {
-            var page = new com_String();
-            // add Except Input
-            page.appendLine('<div class="vp-bold" style="margin-top:5px;">Except</div>');
-            page.appendFormatLine('<input type="text" class="vp-input wp100 vp-state {0}" value="{1}" placeholder="{2}">'
-                                    ,this.exceptClassName , this.inputBoxText, this.inputBoxText);
-            // add +except btn
-            page.appendFormatLine('<button class="vp-button w90" id="{0}">+ Except</button>', this.addBtnIdName);
-            return page.toString();
+            return '';
         }
 
         generateCode() {
-            // generate code new!
             return 'try:';
         }
 
