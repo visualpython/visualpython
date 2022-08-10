0c91b7c8fd25c9623cef3604fa479e1d077d4b04
minjk-bl <mjkim-bl@gmail.com>
2022-08-11 03:01:43 +0900

    CHROME: Fixed api to insert cells

--- js/com/com_interface.js
+++ js/com/com_interface.js
@@ -14,7 +14,15 @@ define([
 ], function(com_util, com_String) {
 
     var getSelectedCell = function() {
-        return Jupyter.notebook.get_selected_index();
+        if (vpConfig.extensionType === 'notebook') {
+            return Jupyter.notebook.get_selected_index();
+        } else if (vpConfig.extensionType === 'chrome') {
+            if (colab.global.notebook.focusedCell) {
+                return colab.global.notebook.focusedCell.cellId;
+            } else {
+                return '';
+            }
+        }
     }
 
     /**
@@ -25,9 +33,6 @@ define([
      * @param {int} sigNum 
      */
     var insertCell = function(type, command, exec=true, sigText='') {
-        var selectedIndex = getSelectedCell();
-        var targetCell = Jupyter.notebook.insert_cell_below(type, selectedIndex);
-
         // Add signature
         if (type == 'code') {
             if (sigText !== '') {
@@ -36,44 +41,11 @@ define([
                 command = '# Visual Python\n' + command;
             }
         }
-        targetCell.set_text(command);
-        Jupyter.notebook.select_next();
-        if (exec) {
-            switch (type) {
-                case "markdown":
-                    targetCell.render();
-                    break;
-                case "code":
-                default:
-                    targetCell.execute();
-            }
-        }
-        // move to executed cell
-        Jupyter.notebook.scroll_to_cell(Jupyter.notebook.get_selected_index());
 
-        com_util.renderSuccessMessage('Your code is successfully generated.');
-    }
-
-    /**
-     * Insert multiple cells
-     * @param {String} type 
-     * @param {Array} commands 
-     * @param {boolean} exec 
-     * @param {int} sigNum 
-     */
-    var insertCells = function(type, commands, exec=true, sigText='') {
-        var selectedIndex = getSelectedCell();
-        var targetCell = Jupyter.notebook.insert_cell_below(type, selectedIndex);
-
-        commands && commands.forEach((command, idx) => {
-            // Add signature
-            if (type == 'code') {
-                if (sigText !== '') {
-                    command = com_util.formatString('# Visual Python: {0}\n', sigText) + command;
-                } else {
-                    command = com_util.formatString('# Visual Python') + command;
-                }
-            }
+        if (vpConfig.extensionType === 'notebook') {
+            var selectedIndex = getSelectedCell();
+            var targetCell = Jupyter.notebook.insert_cell_below(type, selectedIndex);
+    
             targetCell.set_text(command);
             Jupyter.notebook.select_next();
             if (exec) {
@@ -86,25 +58,116 @@ define([
                         targetCell.execute();
                 }
             }
+            // move to executed cell
+            Jupyter.notebook.scroll_to_cell(Jupyter.notebook.get_selected_index());
+        } else if (vpConfig.extensionType === 'chrome') {
+            // CHROME: use colab api to add cell
+            colab.global.notebook.notebookToolbar.toolbarButtons.get("insert-cell-below").click();
+            let cell = colab.global.notebook.focusedCell;
+            cell.setText(command);
+            if (exec) {
+                switch (type) {
+                    case "markdown":
+                        // trigger esc
+                        var esc = $.Event("keydown", { keyCode: 27 });
+                        // cell.trigger(esc); // CHROME: FIXME:
+                        console.log('this is your cell', cell);
+                        break;
+                    case "code":
+                    default:
+                        cell.runButton.click();
+                }
+            }
+            // move to executed cell
+            // CHROME: TODO:
+        }
 
-            selectedIndex = getSelectedCell();
-            targetCell = Jupyter.notebook.insert_cell_below(type, selectedIndex);
+        com_util.renderSuccessMessage('Your code is successfully generated.');
+    }
+
+    /**
+     * Insert multiple cells
+     * @param {String} type 
+     * @param {Array} commands 
+     * @param {boolean} exec 
+     * @param {int} sigNum 
+     */
+    var insertCells = function(type, commands, exec=true, sigText='') {
+        
+        commands && commands.forEach((command, idx) => {
+            // Add signature
+            if (type == 'code') {
+                if (sigText !== '') {
+                    command = com_util.formatString('# Visual Python: {0}\n', sigText) + command;
+                } else {
+                    command = com_util.formatString('# Visual Python') + command;
+                }
+            }
+
+            if (vpConfig.extensionType === 'notebook') {
+                var selectedIndex = getSelectedCell();
+                var targetCell = Jupyter.notebook.insert_cell_below(type, selectedIndex);
+
+                targetCell.set_text(command);
+                Jupyter.notebook.select_next();
+                if (exec) {
+                    switch (type) {
+                        case "markdown":
+                            targetCell.render();
+                            break;
+                        case "code":
+                        default:
+                            targetCell.execute();
+                    }
+                }
+            } else if (vpConfig.extensionType === 'chrome') {
+                // CHROME: use colab api to add cell
+                colab.global.notebook.notebookToolbar.toolbarButtons.get("insert-cell-below").click();
+                let cell = colab.global.notebook.focusedCell;
+                cell.setText(command);
+                if (exec) {
+                    switch (type) {
+                        case "markdown":
+                            // trigger esc
+                            var esc = $.Event("keydown", { keyCode: 27 });
+                            // cell.trigger(esc); // CHROME: FIXME:
+                            console.log('this is your cell', cell);
+                            break;
+                        case "code":
+                        default:
+                            cell.runButton.click();
+                    }
+                }
+            }
         });
         
         // move to executed cell
-        Jupyter.notebook.scroll_to_cell(Jupyter.notebook.get_selected_index());
+        if (vpConfig.extensionType === 'notebook') {
+            Jupyter.notebook.scroll_to_cell(Jupyter.notebook.get_selected_index());
+        } else if (vpConfig.extensionType === 'chrome') {
+            // CHROME: TODO:
+            
+        }
 
         com_util.renderSuccessMessage('Your code is successfully generated.');
     }
     
     var enableOtherShortcut = function() {
         vpLog.display(VP_LOG_TYPE.DEVELOP, 'enable short cut');
-        Jupyter.notebook.keyboard_manager.enable();
+        if (vpConfig.extensionType == 'notebook') {
+            Jupyter.notebook.keyboard_manager.enable();
+        } else if (vpConfig.extensionType == 'chrome') {
+            ;
+        }
     }
 
     var disableOtherShortcut = function() {
         vpLog.display(VP_LOG_TYPE.DEVELOP, 'disable short cut');
-        Jupyter.notebook.keyboard_manager.disable();
+        if (vpConfig.extensionType == 'notebook') {
+            Jupyter.notebook.keyboard_manager.disable();
+        } else if (vpConfig.extensionType == 'chrome') {
+            ;
+        }
     }
     
     return {
