bb15d082656fe9a070ee509c7508bb9328a19994
minjk-bl <mjkim-bl@gmail.com>
2023-07-07 12:05:13 +0900

    Add package manager config file

--- visualpython/js/com/com_Kernel.js
+++ visualpython/js/com/com_Kernel.js
@@ -44,6 +44,9 @@ define([
                 },
                 'vpimport': {
 
+                },
+                'vppackman': {
+
                 }
             }
         }
@@ -697,6 +700,9 @@ define([
                 case 'vpimport':
                     configFile = 'import';
                     break;
+                case 'vppackman':
+                    configFile = 'packman';
+                    break;
             }
             var cmd = com_util.formatString("print(_vp_get_colab_vpcfg('{0}'))", configFile);
             return new Promise(function(resolve, reject) {
@@ -728,6 +734,9 @@ define([
                     break;
                 case 'vpimport':
                     configFile = 'import';
+                    break;
+                case 'vppackman':
+                    configFile = 'packman';
                     break;
             }
             // write file
@@ -763,6 +772,9 @@ define([
                 case 'vpimport':
                     configFile = 'import';
                     break;
+                case 'vppackman':
+                    configFile = 'packman';
+                    break;
             }
             var cmd = com_util.formatString("print(_vp_get_lab_vpcfg('{0}'))", configFile);
             return new Promise(function(resolve, reject) {
@@ -794,6 +806,9 @@ define([
                     break;
                 case 'vpimport':
                     configFile = 'import';
+                    break;
+                case 'vppackman':
+                    configFile = 'packman';
                     break;
             }
             // write file
